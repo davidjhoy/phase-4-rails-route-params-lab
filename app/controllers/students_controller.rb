0860dc0b65b08f6d87cfd0218a4dc3a658b6a44d
david <davidjhoyjr@gmail.com>
2022-07-16 13:02:05 -0400

    commit tree d94fb46f4c4cd053bbc8ae891160869f758d7a9a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,25 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # if 
+    if params.key?("name") == true
+      
+       
+      render json: Student.where(first_name: params[:name].capitalize).or(Student.where(last_name: params[:name].capitalize))
+    
+    else
+      students = Student.all
+      render json: students
+  #
+    end
+ 
   end
+
+  def ids 
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  
 
 end
